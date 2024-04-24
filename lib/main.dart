073bc9aa5f53ed54f6c8ac1172a60e6b044645e3
Pxrit <parit.pongsai@g.swu.ac.th>
2024-04-25 02:54:16 +0700

    change diaolg

--- lib/main.dart
+++ lib/main.dart
@@ -6,22 +6,24 @@ import 'package:maptes/src/views/fullscreenmap.dart';
 
 void main() => runApp(MainApp());
 
-class MainApp extends StatelessWidget {
-  
+class MainApp extends StatefulWidget {
+  @override
+  _MainAppState createState() => _MainAppState();
+}
+
+class _MainAppState extends State<MainApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Map Test',
       home: Scaffold(
-        appBar: AppBar(
-          title: Text('Map Test'),
-        ),
         body: Center(
           child: mapshowpage(),
         ),
       ),
     );
   }
+  
 }
 
--- lib/src/views/fullscreenmap.dart
+++ lib/src/views/fullscreenmap.dart
@@ -10,95 +10,141 @@ class mapshowpage extends StatefulWidget {
   _mapshowpageState createState() => _mapshowpageState();
 }
 
-class _mapshowpageState extends State<mapshowpage>{
-
+class _mapshowpageState extends State<mapshowpage> {
   List<Marker> allMarkers = [];
-  MapController mapController = MapController();
 
-  @override 
+  @override
   void initState() {
     // TODO: implement initState
     super.initState();
     loadMarkers();
   }
 
-  @override 
+  @override
   void dispose() {
     super.dispose();
   }
-Future<void> loadMarkers() async {
-    String jsonString = await rootBundle.loadString('assets/json/markerdata.json');
+
+  Future<void> loadMarkers() async {
+    String jsonString =
+        await rootBundle.loadString('assets/json/markerdata.json');
     List<dynamic> rawMarkers = jsonDecode(jsonString);
-    allMarkers = rawMarkers.map((markerJson) => Marker(
-      width: 80.0,
-      height: 80.0,
-      point: LatLng(markerJson['lat'] as double, markerJson['lng'] as double), 
-      child: GestureDetector(
-        onTap: () {
-          showDialog(
-            context: context,
-            builder: (BuildContext context) {
-              return AlertDialog(
-                title: Text('Marker Info'),
-                content: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text('Latitude: ${markerJson['lat']}'),
-                    Text('Longitude: ${markerJson['lng']}'),
-                    Text('Name: ${markerJson['name']}'),
-                  ],
-                ),
-                actions: [
-                  TextButton(
-                    child: Text('Close'),
-                    onPressed: () {
-                      Navigator.of(context).pop();
-                    },
-                  ),
+    allMarkers = rawMarkers
+        .map((markerJson) => Marker(
+            width: 80.0,
+            height: 80.0,
+            point: LatLng(
+                markerJson['lat'] as double, markerJson['lng'] as double),
+            child: GestureDetector(
+              onTap: () {
+                showDialog(
+                  context: context,
+                  builder: (BuildContext context) {
+                    return AlertDialog(
+                      title: Text('Marker Info'),
+                      content: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text('Latitude: ${markerJson['lat']}'),
+                          Text('Longitude: ${markerJson['lng']}'),
+                          Text(
+                            'Building: ${markerJson['Building']}',
+                          ),
+                          Text('Name: ${markerJson['Name']}'),
+                        ],
+                      ),
+                      actions: [
+                        TextButton(
+                          child: Text('Close'),
+                          onPressed: () {
+                            Navigator.of(context).pop();
+                          },
+                        ),
+                      ],
+                    );
+                  },
+                );
+              },
+              child: Column(
+                children: [
+                  Icon(Icons.location_on, size: 45.0, color: Colors.green),
                 ],
-              );
-            },
-          );
-        },
-        child: Column(
-          children: [
-            Icon(Icons.location_on, size: 45.0, color: Colors.red),
-          ],
-        ),
-      )
-    )).toList();
+              ),
+            )))
+        .toList();
     setState(() {
       allMarkers = allMarkers;
     });
-}
+  }
 
+  MapController? mapController;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-       body: Stack(
+      body: Stack(
         children: [
           FlutterMap(
             options: MapOptions(
-              center: LatLng(13.745152201874209, 100.56455251503306),
-              zoom: 17.2,
-             // swPanBoundary: LatLng(13.744152201874209, 100.56355251503306),
-             // nePanBoundary: LatLng(13.746152201874209, 100.56555251503306
+              center: LatLng(13.745322833337358, 100.56528296835661),
+              zoom: 17.0,
+              minZoom: 17.0,
+              maxZoom: 19.0,
+              enableMultiFingerGestureRace: true,
+              //disble interact
+              interactiveFlags: InteractiveFlag.doubleTapDragZoom |
+                    InteractiveFlag.doubleTapZoom |
+              InteractiveFlag.flingAnimation |
+              InteractiveFlag.pinchZoom |
+              InteractiveFlag.scrollWheelZoom,    
+              rotation: -15.0, 
             ),
             children: [
               TileLayer(
-                  urlTemplate: 'https://api.mapbox.com/styles/v1/noticszero/cluzqroj7005m01pk5dk0cx29/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1Ijoibm90aWNzemVybyIsImEiOiJjbHV6cW1udHAxZW9zMmtudzdzaTdpaGp0In0.biD8--EXRF9VUcLqY-9x5g',
-                  userAgentPackageName: 'https://studio.mapbox.com/',
-              ),  
-                MarkerLayer(
-                markers: allMarkers
+                urlTemplate:
+                    'https://api.mapbox.com/styles/v1/noticszero/cluzqroj7005m01pk5dk0cx29/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1Ijoibm90aWNzemVybyIsImEiOiJjbHV6cW1udHAxZW9zMmtudzdzaTdpaGp0In0.biD8--EXRF9VUcLqY-9x5g',
+                userAgentPackageName: 'https://studio.mapbox.com/',
+              ),
+              MarkerLayer(
+                markers: allMarkers,
               ),
             ],
-            
+          ),
+          Positioned(
+            top: 0,
+            left: 0,
+            right: 0,
+            child: AppBar(
+              backgroundColor: Colors.white,
+              title: TextField(
+                decoration: InputDecoration(
+                  hintText: 'Search',
+                  hintStyle: TextStyle(color: Colors.white),
+                ),
+                style: TextStyle(color: Colors.white),
+              ),
+            ),
+          ),
+          Positioned(
+            bottom: 0,
+            left: 0,
+            right: 0,
+            child: Container(
+              height: 50,
+              color: Colors.blue,
+              child: Center(
+                child: Text(
+                  'Bottom Bar',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+            ),
           ),
         ],
       ),
     );
   }
-
-
 }
