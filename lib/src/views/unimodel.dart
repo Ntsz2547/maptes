04b9ff7e796b330453d687b4f35727d5d5161989
tantong <thaenthong.plengsiang@g.swu.ac.th>
2024-05-05 17:26:56 +0700

    Home page Gui

--- lib/src/views/unimodel.dart
+++ lib/src/views/unimodel.dart
@@ -11,41 +11,59 @@ class _LayerMapState extends State<LayerMap> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Maptes'),
-      ),
-      body: Container(
-        child: Stack(
-          children: [
-            InteractiveViewer(
-              minScale: 10,
-              maxScale: 20,
-              child: const ModelViewer(
-                src: 'assets/maptest.glb',
-                alt: "MAPMOR",
-                cameraControls: true,
-                autoRotate: true,
-                disableZoom: false,
-
-                // Disable camera controls
+      body: Column(
+        children: <Widget>[
+          Container(
+            height: 200,
+            child: Stack(
+              children: <Widget>[
+                Positioned(
+                    child: Container(
+                  decoration: BoxDecoration(
+                      image: DecorationImage(
+                    image: AssetImage('assets/logo.png'),
+                  )),
+                ))
+              ],
+            ),
+          ),
+          Container(
+            height: 400,
+            child: ModelViewer(
+              src: 'assets/maptest.glb',
+              alt: "MAPMOR",
+              cameraControls: true,
+              autoRotate: true,
+              disableZoom: false,
+              // Disable camera controls
+            ),
+          ),
+          Container(
+            height: 55.0,
+            width: 200.0,
+            // decoration: BoxDecoration(
+            //   borderRadius: BorderRadius.circular(10.0),
+            //   color: Color.fromRGBO(218, 33, 40, 1.0),
+            // ),
+            margin: EdgeInsets.symmetric(horizontal: 50.0),
+            child: ElevatedButton(
+              onPressed: () {
+                Navigator.of(context).push(
+                  MaterialPageRoute(
+                    builder: (context) => mapshowpage(),
+                  ),
+                );
+              },
+              child: Text('Get Started'),
+              style: ButtonStyle(
+                backgroundColor: MaterialStateProperty.all<Color>(
+                  Color.fromRGBO(218, 33, 40, 1.0),
+                ), // Change the color here
+                foregroundColor: MaterialStateProperty.all<Color>(Colors.white),
               ),
             ),
-            Positioned(
-              bottom: 16.0,
-              right: 16.0,
-              child: ElevatedButton(
-                onPressed: () {
-                  Navigator.of(context).push(
-                    MaterialPageRoute(
-                      builder: (context) => mapshowpage(),
-                    ),
-                  );
-                },
-                child: Text('Get Started'),
-              ),
-            ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
